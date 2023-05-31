8424870b2aaf8b4fcc421b9eff768ffac8423348
Raphael <raphael@wrathcodes.com>
2023-05-31 16:06:19 -0300

    fix: restored in-memory-addresses-repository.ts

--- src/repositories/in-memory/in-memory-addresses-ropository.ts
+++ src/repositories/in-memory/in-memory-addresses-ropository.ts
@@ -15,11 +15,33 @@ export class InMemoryAddressesRepository implements AddressesRepository {
       state: data.state,
       zipcode: data.zipcode,
       created_at: new Date(),
-      org_id: data.org_id,
+      org_id: data.org_id ?? null,
     }
 
     this.items.push(address)
 
+    return address
+  }
+
+  async findByOrgId(org_id: string): Promise<Address | null> {
+    const address = this.items.find((address) => address.org_id === org_id)
+    if (!address) return null
+    return address
+  }
+
+  async findByCity(city: string): Promise<Address[]> {
+    const addresses = this.items.filter((address) => address.city === city)
+    if (!addresses) return []
+    return addresses
+  }
+
+  async setAddressOrgId(
+    org_id: string,
+    address_id: string,
+  ): Promise<Address | null> {
+    const address = this.items.find((address) => address.id === address_id)
+    if (!address) return null
+    address.org_id = org_id
     return address
   }
 }
